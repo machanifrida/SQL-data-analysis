81488652ed40d6ebf0e0b57c8e6f32f4f58470d8
Fridah Gechemba Machani <134602815+machanifrida@users.noreply.github.com>
2025-06-03 09:45:24 +0200

    SQL create tables script.sql

---
+++ SQL create tables script.sql
@@ -0,0 +1,55 @@
+DROP TABLE IF EXISTS coffeesales;
+
+CREATE TABLE coffeesales(
+	sale_date DATE,
+	date_time TIMESTAMP,
+	payment_mode TEXT,
+	card TEXT,
+	amount_paid NUMERIC,
+	coffee_name TEXt
+);
+
+DROP TABLE IF EXISTS amazon_categories;
+DROP TABLE IF EXISTS amazon_products;
+
+CREATE TABLE amazon_categories(
+	id Numeric,
+	category_name TEXT
+);
+
+CREATE TABLE amazon_products(
+	asin TEXT,
+	title TEXT,
+	stars FLOAT,
+	reviews NUMERIC,
+	price FLOAT,
+	listprice FLOAT,
+	category_id NUMERIC,
+	isBestSeller TEXT,
+	boughtInLastMonth NUMERIC
+);
+
+DROP TABLE IF EXISTS digital_marketing;
+
+CREATE TABLE digital_marketing(
+	CustomerID NUMERIC,
+	Age NUMERIC,
+	Gender TEXT,
+	Income NUMERIC,
+	CampaignChannel TEXT,
+	CampaignType TEXT,
+	AdSpend FLOAT,
+	ClickThroughRate FLOAT,
+	ConversionRate FLOAT,
+	WebsiteVisits NUMERIC,
+	PagesPerVisit FLOAT,
+	TimeOnSite FLOAT,
+	SocialShares NUMERIC,
+	EmailOpens NUMERIC,
+	EmailClicks NUMERIC,
+	PreviousPurchases NUMERIC,
+	LoyaltyPoints NUMERIC,
+	AdvertisingPlatform TEXT,
+	AdvertisingTool TEXT,
+	Conversion NUMERIC
+);
